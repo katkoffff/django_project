d5d1e5195ec804bd206a4ddacde53747c831e551
Nikolay Katkov <71480869+katkoffff@users.noreply.github.com>
2021-01-11 08:54:26 +0500

    Add files via upload

---
+++ np_mod_04/np/news/views.py
@@ -0,0 +1,60 @@
+#from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Post
+import datetime
+from django.core.paginator import Paginator
+from .filters import NewsFilter
+from .forms import PostForm
+
+class NewsList(ListView):
+    model = Post
+    template_name = 'news_app/news.html'
+    context_object_name = 'news'
+    ordering = ['-create_post']
+    paginate_by = 1
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = datetime.datetime.utcnow()
+        #context['filter'] = NewFilter(self.request.GET, queryset=self.get_queryset())
+        #context['categories'] = Category.objects.all()
+        return context
+
+class NewsDetail(DetailView):
+    template_name = 'news_app/detail.html'
+    context_object_name = 'news_detail'
+    queryset = Post.objects.all()
+
+class NewsCreate(CreateView):
+    template_name = 'news_app/add.html'
+    context_object_name = 'news_create'
+    form_class = PostForm
+    success_url = '/news/'
+
+class NewsUpdate(UpdateView):
+    template_name = 'news_app/edit.html'
+    context_object_name = 'news_update'
+    form_class = PostForm
+    success_url = '/news/'
+
+    def get_object(self, **kwargs):
+        id = self.kwargs.get('pk')
+        return Post.objects.get(pk=id)
+
+class NewsDelete(DeleteView):
+    template_name = 'news_app/delete.html'
+    context_object_name = 'news_delete'
+    queryset = Post.objects.all()
+    success_url = '/news/'
+
+class NewsSearch(ListView):
+    template_name = 'news_app/search.html'
+    context_object_name = 'news_search'
+    ordering = ['-create_post']
+    queryset = Post.objects.all()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = datetime.datetime.utcnow()
+        context['filter'] = NewsFilter(self.request.GET, queryset=self.get_queryset())
+        return context
